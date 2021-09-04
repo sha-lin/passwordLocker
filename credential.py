99815b5647e2a1ec97d15528a2aea1fc0564e876
shalin <shalin.rono@student.moringaschool.com>
2021-09-04 11:47:03 +0300

    create a method and a credentials array

--- credential.py
+++ credential.py
@@ -1,4 +1,11 @@
 class Credential:
     pass
     credentials_array = []
-    
+
+    def __init__(self,user_name,password,email):
+        self.user_name = user_name
+        self.password = password
+        self.email = email
+
+    def sa    
+        pass
