11d374cf0302efea456f1a03ccacdb0b5eff6d11
shalin <shalin.rono@student.moringaschool.com>
2021-09-04 11:23:56 +0300

    test new user objects

--- user_test.py
+++ user_test.py
@@ -6,3 +6,13 @@ class TestUser(unittest.TestCase):
     def setUp(self):
 
         self.new_user = User("Shalin Rono","ronoshalin@gmail.com","0701338496")
+    def tearDown(self):
+
+        User.user_array = []
+
+    def test_init(self):
+
+        self.assertEqual(self.new_user.full_name,"Shalin Rono")
+        self.assertEqual(self.new_user.email,"ronoshalin@gmail.com")
+        self.assertEqual(self.new_user.mobile_number,"0701338496")
+                
