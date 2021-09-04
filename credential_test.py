0bf360e72b56363832653a5faa49cad1afe76e82
shalin <shalin.rono@student.moringaschool.com>
2021-09-04 13:20:59 +0300

    save new user's credentials

--- credential_test.py
+++ credential_test.py
@@ -6,4 +6,18 @@ class TestUser(unittest.TestCase):
     def setUp(self):
 
         self.new_credential = Credential("user_name", "password","email@gmail.com")
-        
+
+    def tearDown(self):
+
+        Credential.credentials_array = []
+
+    def test_init(self):
+
+        self.assertEqual(self.new_credential.user_name, "user_name")
+        self.assertEqual(self.new_credential.password, "password")
+        self.assertEqual(self.new_credential.email, "email@gmail.com")
+
+    def test_save_cred(self):
+
+        self.new_credential.save_credentials()
+        self.assertEqual(len(Credential.credentials_array), 1)
