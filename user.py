0a5cc9d37dcdbaee2e9da3c227ab4ebe7647c13d
shalin <shalin.rono@student.moringaschool.com>
2021-09-04 11:08:14 +0300

    create user functions and methods

--- user.py
+++ user.py
@@ -3,3 +3,22 @@ class User:
     creates new user instances
     """
     pass
+
+    user_array = []
+
+    def __init__(self,full_name,email,mobile_number):
+        self.full_name = full_name
+        self.email = email
+        self.mobile_number = mobile_number
+
+    def save_user_details(self):
+
+        User.user_array.append(self)
+
+    @classmethod
+    def display_users(cls):
+
+        return cls.user_array
+
+
+        pass
