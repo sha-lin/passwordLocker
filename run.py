fae8a232c7caf3f2f14191cf3426122ba6a3f686
shalin <shalin.rono@student.moringaschool.com>
2021-09-04 14:35:05 +0300

    create new credentials fumction

--- run.py
+++ run.py
@@ -6,3 +6,9 @@ def create_user(fullname,email,mobilenumber):
 
     new_user = User(fullname,email,mobilenumber)
     return new_user
+
+
+def create_credential(username, psword, email):
+
+    new_credential = Credential(username,psword,email)
+    return new_credential    
